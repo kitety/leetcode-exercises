af9a91b7d1939ea53654cc1a9b0776e04895fb39
kitety <aizaizuori@foxmail.com>
2019-03-05 20:39:19 +0800

    题目更新

--- 7.整数反转.js
+++ 7.整数反转.js
@@ -12,54 +12,56 @@
  * Testcase Example:  '123'
  *
  * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
- * 
+ *
  * 示例 1:
- * 
+ *
  * 输入: 123
  * 输出: 321
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: -123
  * 输出: -321
- * 
- * 
+ *
+ *
  * 示例 3:
- * 
+ *
  * 输入: 120
  * 输出: 21
- * 
- * 
+ *
+ *
  * 注意:
- * 
+ *
  * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
  * 0。
- * 
+ *
  */
 /**
  * @param {number} x
  * @return {number}
  */
-var reverse = function (x) {
-    let isLessThan0 = false
-    // 转为字符串
-    x = x + '';
-    x = x.split('')
-    if (x[0] === '-') {
-        x.splice(0, 1);
-        isLessThan0 = true
-    }
-    while (x[x.length - 1] == '0') {
-        x.splice(x.length - 1, 1);
-    }
-    x = Number(x.reverse().join(''))
-    x = isLessThan0 ? 0 - x : x;
-    if (x > 2147483647 || x < -2147483648) {
-        x = 0
-    }
-    return x;
+var reverse = function(x) {
+  let isLessThan0 = false;
+  // 转为字符串
+  x = x + "";
+  x = x.split("");
+  // 是不是负数
+  if (x[0] === "-") {
+    x.splice(0, 1);
+    isLessThan0 = true;
+  }
+  // 移除末尾的0
+  while (x[x.length - 1] == "0") {
+    x.splice(x.length - 1, 1);
+  }
+  x = Number(x.reverse().join(""));
+  x = isLessThan0 ? 0 - x : x;
+  // 判断范围
+  if (x > 2147483647 || x < -2147483648) {
+    x = 0;
+  }
+  return x;
 };
 
-
-console.log(reverse(-123))
+console.log(reverse(-123));
