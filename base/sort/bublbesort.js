bf3aba9c8527cbc7591c5ffb286db744acd6dc56
刘正锟 <liuzhengkun@medlinker.com>
2020-12-11 18:06:29 +0800

    feat: 排序

---
+++ base/sort/bublbesort.js
@@ -0,0 +1,18 @@
+
+// 时间 on2 
+Array.prototype.bubbleSort = function () {
+  // this 为数组
+  for (var i = 0; i < this.length - 1; i++) {
+    for (var j = 0; j < this.length - 1 - i; j++) {
+      if (this[j] > this[j + 1]) {
+        const temp = this[j]
+        this[j] = this[j + 1]
+        this[j + 1] = temp
+      }
+    }
+  }
+  return this
+}
+const arr = [8, 7, 8, 2, 3, 6]
+console.log("arr: ", arr)
+console.log("arr.bubbleSort(): ", arr.bubbleSort())
---
+++ base/sort/selectionSort.js
@@ -0,0 +1,20 @@
+Array.prototype.selectionSort = function () {
+  // this 为数组
+  for (let i = 0; i < this.length - 1; i++) {
+    let minIndex = i
+    for (let j = i; j <= this.length - 1; j++) {
+      if (this[j] < this[minIndex]) {
+        minIndex = j
+      }
+    }
+    if (i !== minIndex) {
+      const temp = this[minIndex]
+      this[minIndex] = this[i]
+      this[i] = temp
+    }
+  }
+  return this
+}
+const arr = [8, 7, 8, 2, 3, 6]
+console.log("arr: ", arr)
+console.log("arr.selectionSort(): ", arr.selectionSort())
